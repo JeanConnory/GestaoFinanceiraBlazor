cd935f1b0ce4fb2e24acc6f1769a45072fdaa1de
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-11-14 23:32:15 -0300

    transacao financeira concluida

---
+++ Gestao.Domain/Libraries/Validations/RequiredIfAmountPaidFilledAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestao.Domain.Libraries.Validations;
+
+public class RequiredIfAmountPaidFilledAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        FinancialTransaction transaction = (FinancialTransaction)validationContext.ObjectInstance;
+
+        if (transaction.AmountPay.HasValue)
+        {
+            if (value is null)
+                return new ValidationResult("O campo é obrigatório!", new[] { validationContext.MemberName! });
+        }
+
+        return ValidationResult.Success;
+    }
+}
---
+++ Gestao/Data/Migrations/20241114144544_databaseV9.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestao.Migrations.Data
+{
+    /// <inheritdoc />
+    public partial class databaseV9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "FinancialTransactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "FinancialTransactions",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Documents",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Documents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "FinancialTransactions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "FinancialTransactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+    }
+}
